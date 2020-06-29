9389959708cf6764da20137d23a196377e0c5e6d
smv5047 <smv5047@gmail.com>
2020-06-28 19:31:49 -0500

    completed ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,32 @@
 class RingBuffer:
+
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.buffer_pointer = 0
 
     def append(self, item):
-        pass
+        # Is buffer at capacity?
+        if len(self.storage) < self.capacity:
+            self.storage.insert(len(self.storage), item)
+            return
+        else:
+            self.storage[self.buffer_pointer] = item
+            if self.buffer_pointer < self.capacity:
+                self.buffer_pointer += 1
+            else:
+                self.buffer_pointer = 0
+            return
 
     def get(self):
-        pass
+        return self.storage
+
+
+new_buffer = RingBuffer(5)
+new_buffer.append('a')
+new_buffer.append('b')
+new_buffer.append('c')
+new_buffer.append('d')
+new_buffer.append('e')
+# new_buffer.append('b')
+print(new_buffer.get())
