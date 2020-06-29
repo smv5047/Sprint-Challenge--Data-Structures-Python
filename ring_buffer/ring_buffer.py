0a49d7f38c42eb52bf2a178cbd32d6ad0fbdf429
smv5047 <smv5047@gmail.com>
2020-06-28 20:55:13 -0500

    fixed final test on ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -12,7 +12,7 @@ class RingBuffer:
             return
         else:
             self.storage[self.buffer_pointer] = item
-            if self.buffer_pointer < self.capacity:
+            if self.buffer_pointer < self.capacity-1:
                 self.buffer_pointer += 1
             else:
                 self.buffer_pointer = 0
@@ -23,10 +23,22 @@ class RingBuffer:
 
 
 new_buffer = RingBuffer(5)
-new_buffer.append('a')
-new_buffer.append('b')
-new_buffer.append('c')
-new_buffer.append('d')
-new_buffer.append('e')
+new_buffer.append(1)
+new_buffer.append(2)
+new_buffer.append(3)
+new_buffer.append(4)
+new_buffer.append(5)
+new_buffer.append(6)
+new_buffer.append(7)
+new_buffer.append(8)
+new_buffer.append(9)
+print(new_buffer.buffer_pointer)
+new_buffer.append(45)
+print(new_buffer.buffer_pointer)
+# new_buffer.append(46)
+# new_buffer.append(47)
+# new_buffer.append(48)
+# new_buffer.append(49)
+
 # new_buffer.append('b')
 print(new_buffer.get())
